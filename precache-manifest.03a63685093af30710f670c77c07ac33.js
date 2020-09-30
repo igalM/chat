5f88fcc3562d8cde4760e43914e3975f1ebe5d58
igalM <igalmm@gmail.com>
2020-10-01 00:09:57 +0300

    Updates

---
+++ precache-manifest.03a63685093af30710f670c77c07ac33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b83ef37c3ae04e3f869bfa02082212a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac6f17238aa72d7d1828",
+    "url": "/static/css/main.f07069e2.chunk.css"
+  },
+  {
+    "revision": "02561477775346b420fd",
+    "url": "/static/js/2.bc776f5d.chunk.js"
+  },
+  {
+    "revision": "be76c1072259f9ba78917d9856358ff1",
+    "url": "/static/js/2.bc776f5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac6f17238aa72d7d1828",
+    "url": "/static/js/main.1e7dcbc1.chunk.js"
+  },
+  {
+    "revision": "925240659d9cec2563b2",
+    "url": "/static/js/runtime-main.7861498c.js"
+  }
+]);
